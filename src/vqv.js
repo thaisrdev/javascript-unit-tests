cd2311f15f0dc170849674db417e91317777fb1f
Thais Rosa <tsrosa18@gmail.com>
2022-07-19 17:45:08 -0300

    requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -17,7 +17,16 @@
 
   Caso a função seja chamada sem nenhum parâmetro, o valor undefined deve ser retornado.
 */
-
-const vqv = () => {};
+const vqv = (name, age) => {
+if (typeof name === 'undefined' || typeof age === 'undefined') {
+  return undefined;
+} else {
+  return `Oi, meu nome é ${name}!
+Tenho ${age} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+};
+};
+console.log(vqv('Thais', 27));
 
 module.exports = vqv;
