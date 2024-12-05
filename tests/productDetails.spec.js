ac6d53c70d8070866a374555f4670ed34910d193
Thais Rosa <tsrosa18@gmail.com>
2024-12-05 17:46:47 -0300

    Update productDetails.spec.js

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -1,33 +1,4 @@
 const productDetails = require('../src/productDetails');
-/*
-  A função productDetails recebe duas strings que representam nomes de produtos, e retorna um array contendo dois objetos com os detalhes dos respectivos produtos.
-
-  Parâmetros:
-  - Uma string;
-  - Uma string;
-
-  Comportamento:
-  productDetails('Alcool gel', 'Máscara')
-
-  // Retorna:
-  [
-    {
-      name: 'Alcool gel'
-      details: {
-        productId: 'Alcool gel123'
-      }
-    },
-    {
-      name: 'Máscara'
-      details: {
-        productId: 'Máscara123'
-      }
-    }
-  ]
-
-  Escreva pelo menos cinco testes para essa função para garantir que a implementação de productDetails está correta.
-
-*/
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
